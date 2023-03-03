8fdc594e7d39c7264a8c9152d93748c936fbc582
srijanpratihar <100072338+srijanpratihar@users.noreply.github.com>
2023-03-03 23:43:14 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -6,7 +6,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const express = require('express');
 const multer = require('multer');
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: 'daexfwdsa',
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
