8c36feb297955e99c3afe73303bdf377e603c58d
srijanpratihar <100072338+srijanpratihar@users.noreply.github.com>
2023-03-03 18:46:03 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,3 @@
 const mbcGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapboxtoken = process.MAPBOX_TOKEN;
+const mapboxtoken = 'pk.eyJ1Ijoic3JpamFuMiIsImEiOiJjbGRvMHM2MnMwZW9jM29xcnVsNXBnNDJsIn0.1_A6YyNSYvwetDimMI1OjA';
 const geocoder = mbxGeocoding({ accessToken: mapboxtoken });
