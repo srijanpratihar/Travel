7e0b985852fa01777706f4312e3e1415da558581
srijanpratihar <100072338+srijanpratihar@users.noreply.github.com>
2023-07-24 11:37:38 +0530

    Add files via upload

---
+++ public/js/map.js
@@ -0,0 +1,3 @@
+const mbcGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapboxtoken = 'pk.eyJ1Ijoic3JpamFuMiIsImEiOiJjbGVzbDQ3cWIxN2ZoM3JydjMzaWt3bXhoIn0.N3jrZBaG3TjjbPL80K7_1w';
+const geocoder = mbxGeocoding({ accessToken: mapboxtoken });
